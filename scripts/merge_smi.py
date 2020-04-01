00f2c932eb6c35e48046a9991a4e351c93daca07
Yihui (Ray) Ren <yihui.ren@gmail.com>
2020-04-01 17:08:09 -0400

    include merge_smi.py

---
+++ scripts/merge_smi.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import pandas as pd
+import argparse
+
+def get_pocket_name(s):
+    s = str(s)
+    return s.lstrip("ml.").rstrip("_dock.smi")
+
+def merge_smi(args):
+    """ merge smi files """
+    data_path = Path(args.directory)
+    protein_name = args.protein
+    df = None
+    for fname in data_path.glob("ml."+protein_name+"*_dock.smi"):
+        print(fname)
+        A = pd.read_csv(fname, delimiter='\t')
+        A.rename(columns = {'reg': get_pocket_name(fname)}, inplace=True)
+        if df is None:
+            df = A
+        else:
+            df = pd.merge(df, A, on=['smiles', 'name'])
+
+    return df
+
+
+if __name__ == '__main__':
+    """ merge all pockets with the same protein name"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory",  type=str,
+                        help="Specify the directory name",
+                        required=True)
+    parser.add_argument("protein", type=str,
+                        help="Specify the protein name",
+                        required=True)
+    parser.add_argument("-o", "--output", help="output file name")
+    parser.add_argument("--format", help="output format",
+                        default='csv',
+                        choices=['csv', 'pkl'])
+    args = parser.parse_args()
+
+    df = merge_smi(args)
+    if args.format == "csv":
+        df.to_csv(args.output+".csv")
+    elif args.format == "pkl":
+        df.to_pickle(args.output+".pkl")
