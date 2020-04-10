4e21c34b2928ecc07a42977489081b29fa648dfe
Yihui (Ray) Ren <yihui.ren@gmail.com>
2020-04-10 12:47:35 -0400

    add io redirect back

--- scripts/train_multiclass.sh
+++ scripts/train_multiclass.sh
@@ -10,4 +10,6 @@ python ../main_covid.py ${data_file} nfp \
        --output_dir ${output_dir} \
        --multiclass  \
        --split_seed 7 \
-       -b 32 -e 50 -r $RUN  
+       -b 32 -e 50 -r $RUN \
+       2>${output_dir}${logf%log}err \
+       >${output_dir}${logf}
