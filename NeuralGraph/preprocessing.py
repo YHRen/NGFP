4561b74a6a600299cafad2d40dde3c902d4632bb
Yihui (Ray) Ren <yihui.ren@gmail.com>
2020-04-13 17:34:07 -0400

    throw away smiles with > 6 neighors

--- NeuralGraph/preprocessing.py
+++ NeuralGraph/preprocessing.py
@@ -3,6 +3,7 @@ from rdkit import Chem
 from . import feature
 from tqdm import tqdm
 import torch
+import sys
 
 
 def padaxis(array, new_size, axis, pad_value=0, pad_right=True):
@@ -63,6 +64,8 @@ def tensorise_smiles(smiles, max_degree=5, max_atoms=None, use_tqdm=False):
     edge_tensor = -np.ones((n, max_atoms or 1, max_degree or 1), dtype=int)
 
     if use_tqdm: smiles=tqdm(smiles)
+
+    throw_away = []
     for mol_ix, s in enumerate(smiles):
         mol = Chem.MolFromSmiles(s)
         atoms = mol.GetAtoms()
@@ -85,6 +88,7 @@ def tensorise_smiles(smiles, max_degree=5, max_atoms=None, use_tqdm=False):
         # preallocate array with neighbor lists (indexed by atom)
         connectivity_mat = [[] for _ in atoms]
 
+        throw_away_flag = False
         for bond in bonds:
             # lookup atom ids
             a1_ix = rdkit_ix_lookup[bond.GetBeginAtom().GetIdx()]
@@ -97,9 +101,13 @@ def tensorise_smiles(smiles, max_degree=5, max_atoms=None, use_tqdm=False):
             # If max_degree is exceeded, resize if max_degree=None (auto), else raise
             new_degree = max(a1_neigh, a2_neigh) + 1
             if new_degree > bond_tensor.shape[2]:
-                assert max_degree is None, 'too many neighours ({0}) in molecule: {1}'.format(new_degree, s)
-                bond_tensor = padaxis(bond_tensor, new_degree, axis=2)
-                edge_tensor = padaxis(edge_tensor, new_degree, axis=2, pad_value=-1)
+                # assert max_degree is None, 'too many neighours ({0}) in molecule: {1}'.format(new_degree, s)
+                # bond_tensor = padaxis(bond_tensor, new_degree, axis=2)
+                # edge_tensor = padaxis(edge_tensor, new_degree, axis=2, pad_value=-1)
+                print("throw_away ", mol_ix, s, file=sys.stderr)
+                throw_away.append(mol_ix)
+                throw_away_flag = True
+            if throw_away_flag: continue
 
             # store bond features
             bond_features = np.array(feature.bond_features(bond), dtype=int)
@@ -110,11 +118,17 @@ def tensorise_smiles(smiles, max_degree=5, max_atoms=None, use_tqdm=False):
             connectivity_mat[a1_ix].append(a2_ix)
             connectivity_mat[a2_ix].append(a1_ix)
 
+        if throw_away_flag: continue
+
         # store connectivity matrix
         for a1_ix, neighbours in enumerate(connectivity_mat):
             degree = len(neighbours)
             edge_tensor[mol_ix, a1_ix, : degree] = neighbours
 
+    if len(throw_away) > 0:
+        atom_tensor = np.delete(atom_tensor, throw_away, axis=0)
+        bond_tensor = np.delete(bond_tensor, throw_away, axis=0)
+        edge_tensor = np.delete(edge_tensor, throw_away, axis=0)
     return torch.from_numpy(atom_tensor).float(), \
            torch.from_numpy(bond_tensor).float(), \
            torch.from_numpy(edge_tensor).long()
