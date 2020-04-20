e8a15d0c145b782e489247d2dc95060e8d1dc94f
Yihui (Ray) Ren <yihui.ren@gmail.com>
2020-04-20 14:03:15 -0400

    updated run scripts

--- NeuralGraph/__init__.py
+++
@@ -1,7 +0,0 @@
-from .model import QSAR
-from .dataset import MolData, SmileData
-from .util import tanimoto_similarity
-
-__all__ = ['QSAR', 'MolData', 'SmileData', 'tanimoto_similarity']
-
-#def get_pretrained_model(mode="MPro"):
--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name = "torch_nfp",
-    version = "0.0.1dev",
+    version = "0.0.2dev",
     author = "Yihui Ren",
     author_email = "yren@bnl.gov",
     description = ("An implementation of neural fingerprint"),
     license = "MIT",
     keywords = "example documentation tutorial",
     url = "https://github.com/YHRen/NGFP",
-    packages=['NeuralGraph'],
+    packages=['NeuralGraph',],
     long_description="",
     install_requires=[
         "torch",
