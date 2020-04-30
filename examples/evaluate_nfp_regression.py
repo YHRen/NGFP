29d38f57baba1a2e0d7a155e1952f75f55600439
Yihui (Ray) Ren <yihui.ren@gmail.com>
2020-04-30 15:57:58 -0400

    add more metrics to ADRP regression evaluation

---
+++ examples/evaluate_nfp_regression.py
@@ -0,0 +1,160 @@
+import torch
+import argparse
+import hashlib
+import pandas as pd
+import numpy as np
+import itertools as its
+from tabulate import tabulate
+from torch.utils.data import DataLoader
+from functools import partial
+from tqdm import tqdm
+from pathlib import Path, PurePath
+from warnings import warn
+from rdkit import Chem, DataStructs
+from rdkit.Chem import AllChem
+from sklearn.metrics import (
+    r2_score,
+    mean_absolute_error as mae,
+    mean_squared_error as mse
+)
+from scipy.stats import pearsonr
+try:
+    import NeuralGraph
+except:
+    import sys
+    sys.path.insert(1,str(PurePath(Path.cwd()).parent))
+    sys.path.insert(1,str(PurePath(Path.cwd())))
+from NeuralGraph.dataset import MolData
+from NeuralGraph.util import dev
+
+BSZ = 32  # batch_size
+SHUFFLE_SIG = None  # random shuffle signature
+def split_train_valid_test(n, p=0.8, v=0.1, seed=None):
+    global SHUFFLE_SIG
+    if seed:
+        np.random.seed(seed)
+    idx = np.arange(n)
+    np.random.shuffle(idx)
+    s = int(n*p)
+    t = int(n*v)
+    m = hashlib.sha256()
+    m.update(idx.tobytes())
+    SHUFFLE_SIG = m.hexdigest()
+    # train, valid, test
+    return idx[:s], idx[s:(s+t)], idx[(s+t):]
+
+
+def load_multiclass_csv(data_file, dem=",", target_name=None, sample=None):
+    df = pd.read_csv(data_file, delimiter=dem)
+    df = df.set_index('smiles')
+    if "name" in df.columns: df = df.drop(columns=["name"])
+    if target_name:
+        clms = [clm for clm in df.columns if clm.startswith(target_name)]
+    else:
+        clms = [clm for clm in df.columns]
+    if len(clms) == 0:
+        raise RunTimeError(f"{target_name} not in the dataset")
+        return
+    clms.sort()
+    df = df[clms]
+    df = df.apply(pd.to_numeric, errors='coerce')
+    df = df.fillna(0) # otherwise conflicts with xuefeng's assignment
+    df = df.apply(np.abs) # otherwise different from previous results.
+    if sample is not None:
+        df = df.sample(sample) if isinstance(sample,int) else df.sample(frac=sample)
+    return df.index, df.values, df.columns
+
+
+def try_load_net(model_file=None):
+    model_file = Path(model_file)
+    if model_file.exists() and model_file.is_file():
+        net = torch.load(args.model, map_location=dev)
+    else:
+        raise FileNotFoundError
+    return net.to(dev)
+
+
+def normalize_array(A):
+    mean, std = np.mean(A), np.std(A)
+    def norm_func(X): return (X-mean) / std
+    def restore_func(X): return X * std + mean
+    return norm_func, restore_func
+
+
+def is_valid_smile_for_NFP(sml, max_degree=6):
+    """
+        NFP requires a valid smile string. 
+    """
+    try:
+        mol = Chem.MolFromSmiles(sml)
+        atoms = mol.GetAtoms()
+    except:
+        warn(f"Not a valid SMILE: {sml}")
+        return False
+
+    for atom in atoms:
+        if atom.GetDegree() >= max_degree:
+            warn(f"larger than max degree {max_degree} {sml}")
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    """
+    This program assumes the canonical smile inputs:
+        <three_letter_dataset_short_name>, <molecule_ID_name>, <smiles>
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--input_file", help="choose the input csv file",
+                        type=str, required=True)
+    parser.add_argument("--split_seed", type=int,
+                        help="random seed for splitting dataset")
+    parser.add_argument("--model", help="choose the pretrained model file for nfp\
+                        method.", type=str, required=True)
+    parser.add_argument("--tqdm", help="use tqdm progress bar",
+                        action="store_true")
+    args = parser.parse_args()
+    print("#", args)
+
+    INPUT = Path(args.input_file)
+    if not INPUT.exists(): raise FileNotFoundError
+    SMILES, TARGET, KEYS = load_multiclass_csv(INPUT)
+    print(f"column names {INPUT.stem} with {len(KEYS)} columns:\
+          {KEYS.tolist()}")
+    NCLASS = len(KEYS)
+    net = try_load_net(args.model)
+    train_idx, valid_idx, test_idx = \
+        split_train_valid_test(len(TARGET), seed=args.split_seed)
+    print(f"split_sig: {SHUFFLE_SIG}")
+    norm_func, restore_func = normalize_array(
+        np.concatenate([TARGET[train_idx], TARGET[valid_idx]], axis=0))
+    target = norm_func(TARGET)
+    test_data = MolData(SMILES[test_idx], target[test_idx], use_tqdm=args.tqdm)
+    test_loader = DataLoader(test_data, batch_size=BSZ, shuffle=False)
+    score = net.predict(test_loader)
+    gt = TARGET[test_idx]
+    prd = restore_func(score)
+
+    res_r2  = []
+    res_cor = []
+    res_mae = []
+    res_mse = []
+    for idx, k in enumerate(KEYS):
+        gt_i, prd_i = gt[:, idx], prd[:, idx]
+        res_r2.append(r2_score(gt_i, prd_i))
+        res_cor.append(pearsonr(gt_i, prd_i)[0])
+        res_mae.append(mae(gt_i, prd_i))
+        res_mse.append(mse(gt_i, prd_i))
+    
+    output_df = pd.DataFrame.from_dict({
+        "target": KEYS,
+        "r2_score": res_r2,
+        "corr_coef": res_cor,
+        "mae": res_mae,
+        "mse": res_mse})
+    output_df.set_index("target")
+    table = tabulate(output_df, headers='keys', tablefmt='github',
+                     floatfmt=".4f")
+    print(table)
+    output_df.to_csv('./eval.csv', index=False, float_format="%.4f")
+
--- reports/ADRP_multi_train300k.md
+++ reports/ADRP_multi_train300k.md
@@ -1,14 +1,33 @@
 # Comparing Testing MSE with baseline model
 
+note: baseline model is using traditional fingerprint and a NN model.
+
+### MSE
 target             | baseline test-loss (mse) | NFP test-loss (mse) |
 |------------------|--------------------------|---------------------|
+|ADRP-ADPR_pocket1 | 0.69                     | 0.6180              |
+|ADRP-ADPR_pocket5 | 0.31                     | 0.2624              |
 |ADRP_pocket1      | 0.42                     | 0.3608              |
 |ADRP_pocket12     | 0.39                     | 0.3558              |
 |ADRP_pocket13     | 0.57                     | 0.5267              |
-|ADRP-ADPR_pocket1 | 0.69                     | 0.6180              |
-|ADRP-ADPR_pocket5 | 0.31                     | 0.2624              |
 
-baseline model is using traditional fingerprint and a NN model.
+### R2 Score
+| target              | baseline R2 score | NPF R2 score |
+|---------------------|-------------------|--------------|
+| ADRP-ADPR_pocket1   | 0.71              | 0.7411       |
+| ADRP-ADPR_pocket5   | 0.75              | 0.7932       |
+| ADRP_pocket1        | 0.65              | 0.7033       |
+| ADRP_pocket12       | 0.63              | 0.6621       |
+| ADRP_pocket13       | 0.66              | 0.6920       |
+
+### Pearson Correlation 
+| target              | corr coef |   corr coef |
+|---------------------|-----------|-------------|
+| ADRP-ADPR_pocket1   | 0.84      |      0.8615 |
+| ADRP-ADPR_pocket5   | 0.87      |      0.8917 |
+| ADRP_pocket1        | 0.81      |      0.8396 |
+| ADRP_pocket12       | 0.8       |      0.8138 |
+| ADRP_pocket13       | 0.81      |      0.8338 |
 
 ### Notes
 
@@ -20,3 +39,39 @@ baseline model is using traditional fingerprint and a NN model.
 
 sha256: `195c6b92b450f6898ca21189ab937061ddbdcf52060131949358ef83d63e80c6`
 
+
+#### Other evaluation metrics
+`Namespace(input_file='../dataset/covid19/xuefeng/ADRP_merged.csv', model='../pretrained/ADRP_mergedmulti_class0.pkg', split_seed=7, tqdm=True)`
+
+`split_sig: 195c6b92b450f6898ca21189ab937061ddbdcf52060131949358ef83d63e80c6`
+
+|    | target            |   r2 score |   corr coef |    mae |    mse |
+|----|-------------------|------------|-------------|--------|--------|
+|  0 | ADRP-ADPR_pocket1 |     0.7411 |      0.8615 | 0.6148 | 0.6180 |
+|  1 | ADRP-ADPR_pocket5 |     0.7932 |      0.8917 | 0.3621 | 0.2624 |
+|  2 | ADRP_pocket1      |     0.7033 |      0.8396 | 0.4638 | 0.3608 |
+|  3 | ADRP_pocket12     |     0.6621 |      0.8138 | 0.4611 | 0.3558 |
+|  4 | ADRP_pocket13     |     0.6920 |      0.8338 | 0.5638 | 0.5267 |
+
+
+#### Baseline model
+(Provided by Xuefeng, older version)
+
+|target            | R2 score | CI   | corr coef | mse    |
+|------------------|----------|------|-----------|--------|
+|ADRP-ADPR_pocket1 | 0.71     | 0.82 | 0.84      | 0.69   |
+|ADRP-ADPR_pocket5 | 0.75     | 0.86 | 0.87      | 0.31   |
+|ADRP_pocket1      | 0.65     | 0.8  | 0.81      | 0.42   |
+|ADRP_pocket12     | 0.63     | 0.79 | 0.8       | 0.39   |
+|ADRP_pocket13     | 0.66     | 0.8  | 0.81      | 0.57   |
+
+
+(April. 30. newer version)
+
+| target_name              | r2-score    | CI       | correlation | loss       |
+|--------------------------|-------------|----------|-------------|------------|
+| ADRP-ADPR_pocket1_round1 | 0.7109      | 0.8229   | 0.8432      | 0.6864     |
+| ADRP-ADPR_pocket5_round1 | 0.7525      | 0.8605   | 0.8677      | 0.3104     |
+| ADRP_pocket1_round1      | 0.6693      | 0.8051   | 0.8183      | 0.4016     |
+| ADRP_pocket12_round1     | 0.6433      | 0.7935   | 0.8028      | 0.3720     |
+| ADRP_pocket13_round1     | 0.6673      | 0.8006   | 0.8169      | 0.5645     |
