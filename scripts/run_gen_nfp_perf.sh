09d7029386b4e08949a591b45fc5484d26612f1f
Yihui (Ray) Ren <yihui.ren@gmail.com>
2020-05-16 16:22:02 -0400

    update perf

--- scripts/run_gen_nfp_perf.sh
+++ scripts/run_gen_nfp_perf.sh
@@ -2,5 +2,21 @@
 # python -m torch.utils.bottleneck generate_nfp_profile.py  -i ./dataset/canonical_ANL/DUD.csv -o ./output/DUD/ --model ./pretrained/MPro_mergedmulti_class.pkg --chunk_size 10000 --batch_size 256 --tqdm --dataset_name DUD >bottle_neck.txt
 # python -m cProfile -o cprof.txt generate_nfp_profile.py  -i ./dataset/canonical_ANL/DUD.csv -o ./output/DUD/ --model ./pretrained/MPro_mergedmulti_class.pkg --chunk_size 10000 --batch_size 256 --tqdm --dataset_name DUD >bottle_neck.txt
 #python generate_nfp_profile.py  -i ./dataset/canonical_ANL/DUD.csv -o ./output/DUD/ --model ./pretrained/MPro_mergedmulti_class.pkg --chunk_size 10000 --batch_size 256 --tqdm --dataset_name DUD >bottle_neck.txt
-python -m cProfile -o cprof.txt generate_nfp_profile.py  -i ./dataset/canonical_ANL/DUD.csv -o ./output/DUD/ --model ./pretrained/MPro_mergedmulti_class.pkg --chunk_size 10000 --batch_size 252 --num_workers 6 --tqdm --dataset_name DUD >bottle_neck.txt
+# python -m cProfile -o cprof.txt generate_nfp_profile.py  -i ./dataset/canonical_ANL/DUD.csv -o ./output/DUD/ --model ./pretrained/MPro_mergedmulti_class.pkg --chunk_size 10000 --batch_size 252 --num_workers 6 --tqdm --dataset_name DUD >bottle_neck.txt
 #python generate_nfp_profile.py  -i ./dataset/canonical_ANL/DUD.csv -o ./output/DUD/ --model ./pretrained/MPro_mergedmulti_class.pkg --chunk_size 10000 --batch_size 256 --num_workers 6 --tqdm --dataset_name DUD >bottle_neck.txt
+
+for worker in 6 12 24;
+do
+    for bsz in 256 512 1024 2048;
+    do
+        echo "worker" $worker "bsz" $bsz
+        python generate_nfp_profile.py  \
+            -i ./dataset/canonical_ANL/DUD.csv \
+            -o ./output/DUD/ \
+            --model ./pretrained/MPro_mergedmulti_class.pkg 
+                    --chunk_size 10000 
+                    --batch_size $bsz 
+                    --num_workers $worker
+                    --tqdm --dataset_name DUD >bottle_neck.txt
+    done
+done
