2f67ab8573c1e83fb5afaa4232015b0b89226833
Ray <yihui.ren@gmail.com>
2020-07-13 12:17:44 -0400

    add pretrained finetune

--- NeuralGraph/model.py
+++ NeuralGraph/model.py
@@ -21,7 +21,7 @@ class NeuralFingerPrint(nn.Module):
                                activation=gop_activation)
         self.pool = GraphPool()
         self.to(dev)
-    
+
     def forward(self, atoms, bonds, edges):
         atoms = self.gcn1(atoms, bonds, edges)
         # atoms = self.bn(atoms)
@@ -92,6 +92,7 @@ class MLP(nn.Module):
             score.append(y_.data.cpu())
         score = T.cat(score, dim=0).numpy()
         return score
+
 
 class QSAR(nn.Module):
     def __init__(self, hid_dim, n_class, max_degree=6, \
@@ -167,14 +168,14 @@ class QSAR(nn.Module):
 
         input:
 
-        smiles: a list of SMILE strings of size **n**. 
-        
-        
+        smiles: a list of SMILE strings of size **n**.
+
+
         output:
 
             a numpy matrix of floats of dimension **n-by-hid_dim**, where
             **hid_dim** is the length of nfp.
-            
+
 
         NOTE: make sure the SMILE strings can be fitted into the GPU memory.
 
@@ -204,3 +205,73 @@ class QSAR(nn.Module):
             res = np.float16(res)
         return res
 
+
+class PreFP(nn.Module):
+    """
+        combine a pre-trained NFP model with a MLP
+        while fixing the finger print
+    """
+    def __init__(self, pretrained, hid_dim, n_class):
+        super(PreFP, self).__init__()
+        self.nfp = pretrained
+        self.mlp = MLP(hid_dim, n_class)
+        self.to(dev)
+
+    def forward(self, atoms, bonds, edges):
+        fp = self.nfp(atoms, bonds, edges)
+        out = self.mlp(fp).squeeze()
+        return out
+
+    def fit(self, loader_train, loader_valid, path, \
+            criterion=nn.BCEWithLogitsLoss(), \
+            epochs=1000, early_stop=100, lr=1e-3):
+        #  the only difference from QSAR is that we only train the MLP part.
+        optimizer = optim.Adam(self.mlp(), lr=lr, weight_decay=1e-4)
+        best_loss = np.inf
+        last_saving = 0
+        for epoch in range(epochs):
+            t0 = time.time()
+            for (Ab, Bb, Eb), yb in loader_train:
+                Ab, Bb, Eb, yb = Ab.to(dev), Bb.to(dev), Eb.to(dev), yb.to(dev)
+                optimizer.zero_grad()
+                y_ = self.forward(Ab, Bb, Eb)
+                #ix = yb == yb
+                #yb, y_ = yb[ix], y_[ix]
+                loss = criterion(y_, yb)
+                loss.backward()
+                optimizer.step()
+            loss_valid = self.evaluate(loader_valid, criterion=criterion)
+            print('[Epoch:%d/%d] %.1fs loss_train: %f loss_valid: %f' % (
+                epoch, epochs, time.time() - t0, loss.item(), loss_valid),
+                  file=sys.stderr)
+            if loss_valid < best_loss:
+                T.save(self, path + '.pkg')
+                print('[Performance] loss_valid is improved from %f to %f, Save model to %s' % (
+                    best_loss, loss_valid, path + '.pkg'),
+                      file=sys.stderr)
+                best_loss = loss_valid
+                last_saving = epoch
+            else:
+                print('[Performance] loss_valid is not improved.',
+                      file=sys.stderr)
+            if early_stop is not None and epoch - last_saving > early_stop: break
+        return T.load(path + '.pkg')
+
+    def evaluate(self, loader, criterion=nn.BCEWithLogitsLoss()):
+        loss = 0
+        for (Ab, Bb, Eb), yb in loader:
+            Ab, Bb, Eb, yb = Ab.to(dev), Bb.to(dev), Eb.to(dev), yb.to(dev)
+            y_ = self.forward(Ab, Bb, Eb)
+            #ix = yb == yb
+            #yb, y_ = yb[ix], y_[ix]
+            loss += criterion(y_, yb).item()
+        return loss / len(loader)
+
+    def predict(self, loader):
+        score = []
+        for (Ab, Bb, Eb), yb in loader:
+            Ab, Bb, Eb, yb = Ab.to(dev), Bb.to(dev), Eb.to(dev), yb.to(dev)
+            y_ = self.forward(Ab, Bb, Eb)
+            score.append(y_.data.cpu())
+        score = T.cat(score, dim=0).numpy()
+        return score
--- main_covid_docking_v5.py
+++ main_covid_docking_v5.py
@@ -4,27 +4,41 @@
 """
 
 from pathlib import Path
-from torch.utils.data import DataLoader, Subset
-from NeuralGraph.dataset import MolData, SmileData
-from NeuralGraph.model import QSAR, MLP
+import torch
 import torch.nn as nn
 import pandas as pd
 import numpy as np
 import argparse
+from torch.utils.data import DataLoader, Subset
+
+from NeuralGraph.dataset import MolData, SmileData
+from NeuralGraph.model import QSAR, MLP, PreFP
+from NeuralGraph.util import dev
 
 
 FP_METHODS = ["morgan", "nfp"]
 FP_LEN = 1 << 10  # fingerprint length for circular FP
 
 # derived from the dataset folder structure
-TARGET_CHOICES = ['3CLPro_3', '3CLPro_2', '3CLPro_1', 'CoV_RDB_DA', 'CoV_RDB_AB',
-                  'CoV_RDB_CD', 'CoV_RDB_A_1', 'CoV_RDB_BC', 'PLPRO_2', 'plpro_1',
-                  'NSP15_3_6w01', 'NSP15_2_6w01', 'NSP15_1_6w01', 'DNMT3A_chainA',
-                  'NSUN2_model', 'Mpro-x0104', 'Mpro-x0161', 'Mpro-x0305', 'Mpro-x0107',
-                  'DNMT1_chainA', 'NSUN6', 'adrp_adpr_A', 'NSP15_2_6vww', 'NSP15_1_6vww']
+TARGET_CHOICES = ['3CLPro_3', '3CLPro_2', '3CLPro_1', 'CoV_RDB_DA',
+                  'CoV_RDB_AB', 'CoV_RDB_CD', 'CoV_RDB_A_1', 'CoV_RDB_BC',
+                  'PLPRO_2', 'plpro_1', 'NSP15_3_6w01', 'NSP15_2_6w01',
+                  'NSP15_1_6w01', 'DNMT3A_chainA', 'NSUN2_model', 'Mpro-x0104',
+                  'Mpro-x0161', 'Mpro-x0305', 'Mpro-x0107', 'DNMT1_chainA',
+                  'NSUN6', 'adrp_adpr_A', 'NSP15_2_6vww', 'NSP15_1_6vww']
+
+TARGET_TRAIN = ['3CLPro_2', 'CoV_RDB_CD', '3CLPro_1', 'NSP15_1_6vww',
+                'adrp_adpr_A', 'DNMT3A_chainA', 'NSP15_1_6w01', '3CLPro_3',
+                'NSP15_2_6vww', 'CoV_RDB_A_1', 'DNMT1_chainA', 'Mpro-x0305',
+                'NSUN2_model', 'NSP15_2_6w01', 'Mpro-x0107', 'plpro_1',
+                'PLPRO_2', 'Mpro-x0104']
+
+TARGET_TEST = ['CoV_RDB_BC', 'NSUN6', 'CoV_RDB_DA', 'CoV_RDB_AB', 'Mpro-x0161']
 
 
 def normalize_array(A):
+    """ normalize the target score
+    """
     mean, std = np.mean(A), np.std(A)
     def norm_func(X): return (X-mean) / std
     def restore_func(X): return X * std + mean
@@ -53,12 +67,28 @@ def construct_dataset(data_file, target_name, sample=None, use_tqdm=None):
 def mse(x, y, dim=None):
     return ((x-y)**2).mean(dim)
 
+def create_net(hid_dim, n_class, pre_trained=None):
+    """
+        pre_trained := the pretrained model file path
+    """
+    if pre_trained is None:
+        # create a new QSAR network
+        net = QSAR(hid_dim=128, n_class=n_class)
+    else:
+        if not Path(pre_trained).exists():
+            raise FileNotFoundError
+        prenet = torch.load(pre_trained, map_location=dev)
+        net = PreFP(prenet.nfp, hid_dim=128, n_class=n_class)
 
+    return net
+
+    return net
 def main(args):
     BSZ, RUNS, LR, N_EPOCH = args.batch_size, args.runs, args.lr, args.epochs
     DATAFOLDER = Path(args.datafolder)
     assert DATAFOLDER.exists()
     OUTPUT = args.output_dir+DATAFOLDER.resolve().stem
+    Path(OUTPUT).mkdir(parents=True, exist_ok=True)
 
     # load data
     NCLASS = len(args.target_name)
@@ -81,29 +111,18 @@ def main(args):
     if args.fp_method == FP_METHODS[0]:
         raise NotImplementedError
 
-    #def build_data_net(args, target):
-    #    if args.fp_method == FP_METHODS[0]:
-    #        #""" CFP """
-    #        data = SmileData(SMILES, target, fp_len=FP_LEN, radius=4)
-    #        net = lambda : MLP(hid_dim=FP_LEN, n_class=NCLASS)
-    #        return data, net
-    #    elif args.fp_method == FP_METHODS[1]:
-    #        #""" NFP """
-    #        net = lambda : QSAR(hid_dim=128, n_class=NCLASS)
-    #        data = MolData(SMILES, target, use_tqdm=args.use_tqdm)
-    #        return data, net
-    #    else:
-    #        raise NotImplementedError
-
     res = []
     for _ in range(RUNS):
         train_loader = DataLoader(train_data, batch_size=BSZ,
-                                  shuffle=True, drop_last=True, pin_memory=True)
+                                  shuffle=True, drop_last=True,
+                                  pin_memory=True)
         valid_loader = DataLoader(val_data, batch_size=BSZ,
                                   shuffle=False, pin_memory=True)
         test_loader = DataLoader(test_data, batch_size=BSZ, shuffle=False)
-        net = QSAR(hid_dim=128, n_class=NCLASS)
-        model_path = OUTPUT+str(_)
+        # net = QSAR(hid_dim=128, n_class=NCLASS)
+        net = create_net(hid_dim=128, n_class=NCLASS,
+                         pre_trained=args.fine_tune)
+        model_path = OUTPUT+"_"
         net = net.fit(train_loader, valid_loader, epochs=N_EPOCH,
                       path=model_path,
                       criterion=nn.MSELoss(), lr=LR)
@@ -126,8 +145,6 @@ if __name__ == '__main__':
     parser.add_argument("fp_method", default="nfp", type=str,
                         help="Specify the fingerprint method",
                         choices=FP_METHODS)
-    parser.add_argument("--delimiter", help="choose the delimiter of the smi\
-                        file", type=str, default=",")
     parser.add_argument("--output_dir", help="specify the output directory",
                         type=str, default="./output/")
     parser.add_argument("-b", "--batch-size", help="batch size",
@@ -138,6 +155,8 @@ if __name__ == '__main__':
                         default=5, type=int)
     parser.add_argument("-l", "--lr", help="learning rate",
                         default=1e-3, type=float)
+    parser.add_argument("-f", "--fine-tune", help="load pretrained model",
+                        type=str)  # use pre-trained model on new targets
     parser.add_argument("--target_name", nargs='+',
                         choices=TARGET_CHOICES,
                         help="specify the column name(s)")
