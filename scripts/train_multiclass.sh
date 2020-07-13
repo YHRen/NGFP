56b14a7ae97436712d8f01ce40a1d6bc8447ce39
Ray <yihui.ren@gmail.com>
2020-07-13 12:22:09 -0400

    add pretrained finetune

--- scripts/train_multiclass.sh
+++ scripts/train_multiclass.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 RUN=5
 data_file=$1
+gpu_id=${2:-0}
 output_dir="../output/"
 df="multiclass"
 logf=${data_file##*/}
 logf=${logf%.csv}${df}.log
 echo $data_file $logf
+
+CUDA_VISIBLE_DEVICES=$gpu_id \
 python ../main_covid.py ${data_file} nfp \
        --output_dir ${output_dir} \
        --multiclass  \
